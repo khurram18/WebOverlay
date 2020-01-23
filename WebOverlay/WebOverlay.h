fe42982a691d573f52e2f10d26627dedfbe19e77
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-23 16:39:22 +0500

    Initial framework project

---
+++ WebOverlay/WebOverlay.h
@@ -0,0 +1,19 @@
+//
+//  WebOverlay.h
+//  WebOverlay
+//
+//  Created by Khurram on 23/01/2020.
+//  Copyright © 2020 Example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebOverlay.
+FOUNDATION_EXPORT double WebOverlayVersionNumber;
+
+//! Project version string for WebOverlay.
+FOUNDATION_EXPORT const unsigned char WebOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebOverlay/PublicHeader.h>
+
+

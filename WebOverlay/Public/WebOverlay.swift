273a520e9d8936ab6f7f10667fb7ca0c60db17fb
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-27 12:34:08 +0500

    Listen to device orientation change events

--- WebOverlay/Public/WebOverlay.swift
+++ WebOverlay/Public/WebOverlay.swift
@@ -6,16 +6,17 @@
 //  Copyright © 2020 Example. All rights reserved.
 //
 
+import UIKit
 import Foundation
-
 
 public final class WebOverlay {
   
 private var manager: OverlayManager?
-  
+private var options = [StartOptions: String]()
 public static let sharedInstance = WebOverlay()
   
 public func start(with options: [StartOptions: String]) {
+  self.options = options
   manager = OverlayManager(options)
 }
 
@@ -24,6 +25,19 @@ public func show() {
 }
 
 
-private init(){}
+private init() {
+  listenOrientationChangeNotifications()
+}
   
 } // class WebOverlay
+
+extension WebOverlay {
+private func listenOrientationChangeNotifications() {
+  UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+  NotificationCenter.default.addObserver(self, selector: #selector(onOrientationChange(_:)), name: UIDevice.orientationDidChangeNotification, object: nil)
+}
+@objc private func onOrientationChange(_ notification: Notification) {
+  manager?.close()
+  manager = OverlayManager(options)
+}
+} // extension WebOverlay

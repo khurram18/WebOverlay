9b546ab2d86f652f5fb2cff89aaa0acb2733ed17
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-31 17:02:21 +0500

    Add package file

---
+++ Package.swift
@@ -0,0 +1,13 @@
+import PackageDescription
+let package = Package(
+	name: "WebOverlay",
+	platforms: [.iOS(.v10)]
+    products: [
+        .library(name: "WebOverlay", targets: ["WebOverlay"])
+    ],
+    targets: [
+        .target(
+            name: "WebOverlay",
+            dependencies: [])
+    ]
+)

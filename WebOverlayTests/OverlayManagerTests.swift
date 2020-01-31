a622ff26eb253382c15bbb0546698ca07eb00a50
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-31 11:39:09 +0500

    Add test cases for overlay manager

---
+++ WebOverlayTests/OverlayManagerTests.swift
@@ -0,0 +1,30 @@
+//
+//  OverlayManagerTests.swift
+//  WebOverlayTests
+//
+//  Created by Khurram on 31/01/2020.
+//  Copyright © 2020 Example. All rights reserved.
+//
+@testable import WebOverlay
+import XCTest
+
+class OverlayManagerTests: XCTestCase {
+
+func testShowOverlay() {
+  let sut = OverlayManager(testStartOptions)
+  let showNotificationExpectation = expectation(forNotification: .webOverlayDidShow, object: nil, handler: nil)
+  sut.show(completion: nil)
+  wait(for: [showNotificationExpectation], timeout: 2)
+}
+  
+func testCloseOverlay() {
+  let sut = OverlayManager(testStartOptions)
+  let showNotificationExpectation = expectation(forNotification: .webOverlayDidClose, object: nil, handler: nil)
+  sut.show() {
+    sut.close(completion: nil)
+  }
+  
+  wait(for: [showNotificationExpectation], timeout: 2)
+}
+  
+}
---
+++ WebOverlayTests/testStartOptions.swift
@@ -0,0 +1,15 @@
+//
+//  testStartOptions.swift
+//  WebOverlayTests
+//
+//  Created by Khurram on 31/01/2020.
+//  Copyright © 2020 Example. All rights reserved.
+//
+
+import Foundation
+
+let testStartOptions = [StartOptions.topTitle: "topTitle",
+                        StartOptions.bottomTitle: "bottomTitle",
+                        StartOptions.closeButtonImageName: "closeButtonImageName",
+                        StartOptions.option4: "option4",
+                        StartOptions.option5: "option5"]
